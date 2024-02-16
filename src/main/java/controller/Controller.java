147791ab42e11e5892bddd59efa57e2ef5446468
felipexx71 <felipenunex285@gmail.com>
2024-02-16 12:28:21 -0300

    add new lib for connection on DB and configuration

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,33 +1,42 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.DAO;
+
 /**
  * Servlet implementation class Controller
  */
-@WebServlet(urlPatterns={"/Controller", "/main"})
+@WebServlet(urlPatterns = { "/Controller", "/main" })
 public class Controller extends HttpServlet {
+	DAO dao = new DAO();
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public Controller() {
+		super();
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+
+		if (action.equals("/main")) {
+			doGetListContacts(request, response);
+		}
+
+	}
+
+	protected void doGetListContacts(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+	}
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,30 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+//modulo de conexao
+public class DAO {
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/agenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "felipe";
+	private String password = "071203";
+
+	private Connection conectar() {
+		Connection con = null;
+
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println(con);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+
+	public void testeConexao() {
+		Connection con = conectar();
+	}
 }
