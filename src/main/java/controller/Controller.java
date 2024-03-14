09afafe4cbee71fcdb8f493c3e2bacdcd55f5a5b
felipexx71 <felipenunex285@gmail.com>
2024-03-13 22:22:15 -0300

    add new archives from javabeans and validation of form

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -9,10 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	DAO dao = new DAO();
+	JavaBeans jb = new JavaBeans();
+	
 	private static final long serialVersionUID = 1L;
 
 	public Controller() {
@@ -25,13 +28,25 @@ public class Controller extends HttpServlet {
 
 		if (action.equals("/main")) {
 			doGetListContacts(request, response);
+		} else if (action.equals("/insert")) {
+			newContact(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 
 	}
 
+	// list contacts
 	protected void doGetListContacts(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
 		dao.testeConexao();
+	}
+
+	protected void newContact(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		jb.setNome(request.getParameter("nome"));
+		jb.setNome(request.getParameter("telefone"));
+		jb.setNome(request.getParameter("email"));
 	}
 }
